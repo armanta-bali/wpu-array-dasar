b56ec925a3d4b2bdf97823aafaad03a82e83c2c2
Armanta Bali <armanto.rgfamily@gmail.com>
2025-05-14 15:07:33 +0800

    add two file to learn array

---
+++ array.js
@@ -0,0 +1,49 @@
+// membuat array kosong
+// menambah array
+// let arr =[];
+// arr[0] = "armanta";
+// arr[1] = "iwayan";
+// arr[2] = "ganteng";
+// arr[4] = "koki";
+// console.log(arr);
+
+
+// menghapus isi array
+// arr[0] = "armanta";
+// arr[1] = "iwayan";
+// arr[2] = "ganteng";
+// arr[3] = "koki";
+// arr[1] = undefined; //ini dilakukan scara manual
+// console.log(arr);
+
+// menampilkan isi array menggunakan looping 
+let arr = ["armanto", "ganteng", "pentol"];
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(`Mahasiswa ke- ${i+1} : ${arr[i]}`);
+}
+
+let fruits = ["apple", "banana", "orange", "mango", "kiwi"];
+let [first, second, ...rest] = fruits;
+
+console.log(first);  // "apple"
+console.log(second); // "banana"
+console.log(rest);   // ["orange", "mango", "kiwi"]
+
+// Here's an example of using the split method to create an array of characters:
+
+let str = "hello";
+let charArray = str.split("");
+console.log(charArray); // ["h", "e", "l", "l", "o"]
+
+// The reverse() method is an array method that reverses an array in place. This means it modifies the original array rather than creating a new one. Here's how we can use it:
+let CharArray = ["h", "e", "l", "l", "o"];
+CharArray.reverse();
+console.log(CharArray); // ["o", "l", "l", "e", "h"]
+
+
+// The final step is to convert the reversed array of characters back into a string. We can accomplish this using the join() method. The join() method creates and returns a new string by concatenating all of the elements in an array, separated by a specified separator string. If you want to join the characters without any separator, you can use an empty string as the argument. Here's an example:
+
+let reversedArray = ["o", "l", "l", "e", "h"];
+let reversedString = reversedArray.join("");
+console.log(reversedString); // "olleh"
